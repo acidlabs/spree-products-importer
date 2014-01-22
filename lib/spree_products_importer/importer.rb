8bcf5ef357ad9f2e04bc390259507bafa090555e
vla <vladimir_suarez@hotmail.com>
2014-01-22 13:37:30 -0300

    Add open_spreadsheet method

--- lib/spree_products_importer/importer.rb
+++ lib/spree_products_importer/importer.rb
@@ -1,5 +1,20 @@
+#encoding: utf-8
+require 'roo'
+
 module SpreeProductsImporter
   class Importer
+
     
+      
+    # Receives a file and then returns a Roo object acording the file extension
+    def self.open_spreadsheet(file)
+      case File.extname(file.original_filename)
+      when '.csv' then Roo::Csv.new(file.path, nil, :ignore)
+      when '.xls' then Roo::Excel.new(file.path, nil, :ignore)
+      when '.xlsx' then Roo::Excelx.new(file.path, nil, :ignore)
+      else raise "Unknown file type: #{file.original_filename}"
+      end
+    end
+
   end
 end
