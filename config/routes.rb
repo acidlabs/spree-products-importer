0aa1c17a1c405b127a729296022a2246e8d6747e
vla <vladimir_suarez@hotmail.com>
2014-01-24 14:14:46 -0300

    Add alias to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.routes.draw do
 
-	get  "products/import" => "products#import"
-	post "load_data"       => "products#load_data" 
+	get  "products/import" => "products#import", as: :import_product    
+	post "load_data" => "products#load_data", as: :load_data_product
 
 end
