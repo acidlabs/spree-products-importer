f5e359c665a035dcea21ab035b58f0d6bee3594c
vla <vladimir_suarez@hotmail.com>
2014-01-24 14:16:25 -0300

    Write load_data method

--- app/controllers/spree/admin/product_controller_decorator.rb
+++ app/controllers/spree/admin/product_controller_decorator.rb
@@ -1,9 +1,15 @@
 module Spree
+
   ProductController.class_eval do
 
     def import
-      
+    end
+
+    def load_data
+      success, message = SpreeProductsImporter::Handler.import(params[:file])
+      redirect_to import_product_path, success ? {notice: message} : {notice: message}
     end
 
   end
+
 end
