117eaef5a0029913e669ffb859678e978a885ad9
vla <vladimir_suarez@hotmail.com>
2014-01-24 12:45:54 -0300

    Add new functionalities to Handler class
    - Add validate_product_data method
    - Integrate rest client request (POST) to get_file_data method

--- lib/spree_products_importer/handler.rb
+++ lib/spree_products_importer/handler.rb
@@ -1,22 +1,49 @@
 #encoding: utf-8
 require 'roo'
+require 'rest-client'
 
 module SpreeProductsImporter
 
   class Handler
 
+    @@api_url_base = "http://localhost:3000"
+    @@api_token    = "token"
+
     # Receives a file and the get data from each file row
     def self.get_file_data(file)
-      spreadsheet = open_spreadsheet(file)
-      header = spreadsheet.row(1)
-      (2..spreadsheet.last_row).each do |i|
-        row = Hash[[header, spreadsheet.row(i)].transpose]
-        puts row.inspect
+      spreadsheet   = open_spreadsheet(file)
+      header        = spreadsheet.row(1)
+      products_list = []
+      api_error     = ""
+      success       = true
 
-        # product = find_by_id(row["id"]) || new
-        # product.attributes = row.to_hash.slice(*accessible_attributes)
-        # product.save!
+      # Validates each row element
+      (2..spreadsheet.last_row).each do |i|
+        row            = Hash[[header, spreadsheet.row(i)].transpose]
+        # TODO: Falta hacer el metodo validate_product_data
+        is_valid, data = validate_product_data row
+
+        if is_valid
+          products_list << data
+        else
+          return "An error found at line #{i}: #{attr} is required"
+        end
       end
+
+      # Creates each product with Spree API
+      products_list.each do |product|
+        begin
+          # Create a Product
+          response = RestClient.post "#{@@api_url_base}/products?token=#{@@api_token}", {product: product}.to_json, {content_type: :json, accept: :json}
+          # TODO: Add properties to recently created product
+
+        rescue RestClient::BadRequest => e
+          api_error = e
+          success   = false
+        end
+      end
+
+      return success ? "Products created successfully" : "API error #{e}"
     end
       
     # Receives a file and then returns a Roo object acording the file extension
@@ -27,6 +54,24 @@ module SpreeProductsImporter
       when '.xlsx' then Roo::Excelx.new(file.path, nil, :ignore)
       else raise "Unknown file type: #{file.original_filename}"
       end
+    end
+
+    # Validate each file row according to required attributes
+    def validate_product_data data
+      required_attributes = ["sku", "name", "price"]
+      validated_data = {}
+
+      required_attributes.each do |attr|
+        if row[attr].blank?
+          return [false, "An error found at line #{i}: #{attr} is required"]
+        else
+          # Add key => value to normalized and validated hash
+          validated_data = validated_data.merge(attr.to_sym => row[attr])
+        end
+      end
+
+      #[true, validated_data]
+      [true, validated_data.merge(:shipping_category_id => 1)]
     end
 
   end
