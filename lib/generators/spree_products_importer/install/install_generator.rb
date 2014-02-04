7bd22f0c74fd34293fa1c1c09fe38a4306dc39bd
Roberto Ríos <roberto.rios@trade360.cl>
2014-02-04 13:24:28 -0300

    Add example.xls file

--- lib/generators/spree_products_importer/install/install_generator.rb
+++ lib/generators/spree_products_importer/install/install_generator.rb
@@ -1,8 +1,11 @@
+require 'fileutils'
+
 module SpreeProductsImporter
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
       class_option :auto_run_migrations, :type => :boolean, :default => false
+      source_root File.expand_path('../../../../templates', __FILE__)
 
       def add_javascripts
         append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_products_importer\n"
@@ -12,6 +15,10 @@ module SpreeProductsImporter
       def add_stylesheets
         inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_products_importer\n", :before => /\*\//, :verbose => true
         inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_products_importer\n", :before => /\*\//, :verbose => true
+      end
+
+      def files
+        copy_file('example.xls', 'lib/templates/example.xls')
       end
 
       def add_migrations
