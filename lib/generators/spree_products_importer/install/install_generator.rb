be364eacce49d346bfcaa5d7150ba53ca6929afe
vla <vladimir_suarez@hotmail.com>
2014-02-05 13:11:01 -0300

    Comment unused add_javascripts and add_stylesheets

--- lib/generators/spree_products_importer/install/install_generator.rb
+++ lib/generators/spree_products_importer/install/install_generator.rb
@@ -7,15 +7,15 @@ module SpreeProductsImporter
       class_option :auto_run_migrations, :type => :boolean, :default => false
       source_root File.expand_path('../../../../templates', __FILE__)
 
-      def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_products_importer\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_products_importer\n"
-      end
+      # def add_javascripts
+      #   append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_products_importer\n"
+      #   append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_products_importer\n"
+      # end
 
-      def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_products_importer\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_products_importer\n", :before => /\*\//, :verbose => true
-      end
+      # def add_stylesheets
+      #   inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_products_importer\n", :before => /\*\//, :verbose => true
+      #   inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_products_importer\n", :before => /\*\//, :verbose => true
+      # end
 
       def files
         copy_file('example.xls', 'lib/templates/example.xls')
