b0c8b9c3114435199743c03573d17d72ac0e4df8
mespina <mespina@acid.cl>
2014-09-11 17:16:48 -0300

    Agrego logica en importer

--- lib/spree_products_importer/importer.rb
+++ lib/spree_products_importer/importer.rb
@@ -30,6 +30,7 @@ module SpreeProductsImporter
       begin
         open_spreadsheet
       rescue RuntimeError => e
+        # ToDo - Corregir
         return e.message
       end
       puts I18n.t(:loading_file, scope: [:spree, :spree_products_importer, :logs], filename: @filename, time: Time.now - start) if Spree::Config.verbose
@@ -54,7 +55,11 @@ module SpreeProductsImporter
               mapper.load @spreadsheet, row_index, data
             end
 
+            before_make data
             make_products data
+            make_variants data
+            make_aditionals data
+            after_make data
 
             if Spree::Config.verbose and row_index % Spree::Config[:log_progress_every] == 0
               puts I18n.t(:progress, scope: [:spree, :spree_products_importer, :logs], filename: @filename, time: Time.now - start, row: row_index, rows: @spreadsheet.last_row, data: data)
@@ -88,20 +93,6 @@ module SpreeProductsImporter
       end
     end
 
-    # Defines the hash with default data and structure used to read the data in each row from excel
-    # This allows easy customizations, overwriting this function
-    #
-    # Returns an Hash
-    def default_hash
-      {
-        product: {},      # {attribute1: VALUE_OR_VALUES, attribute2: VALUE_OR_VALUES, ...}
-        variant: {},      # {attribute1: VALUE_OR_VALUES, attribute2: VALUE_OR_VALUES, ...}
-        taxons: [],       # [taxon1, taxon2, ......]
-        properties: [],   # [{property_name1: PROPERTY_VALUE}, {property_name2: PROPERTY_VALUE}, ....]
-        images: [],       # [file_name1, file_name2, ......]
-        aditionals: {}    # {aditional1: ADITIONAL_VALUE_OR_VALUES, aditional2: ADITIONAL_VALUE_OR_VALUES, ....}
-      }
-    end
 
     private
       # Receives a file instance and then returns a Roo object acording the file extension
@@ -121,12 +112,112 @@ module SpreeProductsImporter
         @spreadsheet.default_sheet = @spreadsheet.sheets.first
       end
 
+      # Defines the hash with default data and structure used to read the data in each row from excel
+      # This allows easy customizations, overwriting this function
+      #
+      # Returns an Hash
+      def default_hash
+        {
+          product: {},        # {attribute1: VALUE_OR_VALUES, attribute2: VALUE_OR_VALUES, ...}
+          option_types: [],   # [{attribute1: VALUE_OR_VALUES, ...}, ...]
+          option_values: {},  # {option_type1: [option_value1, option_value2, ...], ...}
+          variants: [],       # [{attribute1: VALUE_OR_VALUES, attribute2: VALUE_OR_VALUES, ...}, ...]
+          aditionals: {},     # {aditional1: ADITIONAL_VALUE_OR_VALUES, aditional2: ADITIONAL_VALUE_OR_VALUES, ....}
+
+
+          taxons: [],       # [taxon1, taxon2, ......]
+          properties: [],   # [{property_name1: PROPERTY_VALUE}, {property_name2: PROPERTY_VALUE}, ....]
+          images: []        # [file_name1, file_name2, ......]
+        }
+      end
+
+      # You must define here your global hash revision logic
+      # To customize the data loaded from the given file, change the hash values, by the appropriate values
+      # You can remove or modify every key into the hash, except those defined in #default_hash, the importer requiere this structure
+      def before_make row
+      end
+
+      # You must define here your hash revision logic
+      # To customize the data loaded from the given file, change the hash values, by the appropriate values
+      # You can remove or modify every key into the hash, except those defined in #default_hash, the importer requiere this structure
+      def before_make_products row
+      end
+
       # Is responsible for creating the Product
       def make_products row
+        before_make_products row
+
         product = Spree::Product.create! row[:product]
 
         # Store the Product :id in the row Hash data
         row[:product][:id] = product.id
+
+        after_make_products row
+      end
+
+      # You must define here your post-insert revision logic
+      # At this point you can check:
+      #   - the data inserted,
+      #   - the final values ​​in the hash data, or
+      #   - perform some required process
+      def after_make_products row
+      end
+
+      # You must define here your hash revision logic
+      # To customize the data loaded from the given file, change the hash values, by the appropriate values
+      # You can remove or modify every key into the hash, except those defined in #default_hash, the importer requiere this structure
+      def before_make_variants row
+      end
+
+      # Is responsible for creating the Product
+      def make_variants row
+        before_make_variants row
+
+        row[:variants].each do |attributes|
+          variant = Spree::Variant.create! attributes
+
+          # Store the Variant :id in the row Hash data
+          attributes[:id] = variant.id
+        end
+
+        after_make_variants row
+      end
+
+      # You must define here your post-insert revision logic
+      # At this point you can check:
+      #   - the data inserted,
+      #   - the final values ​​in the hash data, or
+      #   - perform some required process
+      def after_make_variants row
+      end
+
+      # You must define here your hash revision logic
+      # To customize the data loaded from the given file, change the hash values, by the appropriate values
+      # You can remove or modify every key into the hash, except those defined in #default_hash, the importer requiere this structure
+      def before_make_aditionals row
+      end
+
+      # You must define here your creation logic
+      def make_aditionals row
+        before_make_aditionals
+        # Put your logic at this level
+        after_make_aditionals
+      end
+
+      # You must define here your post-insert revision logic
+      # At this point you can check:
+      #   - the data inserted,
+      #   - the final values ​​in the hash data, or
+      #   - perform some required process
+      def after_make_aditionals row
+      end
+
+      # You must define here your global post-insert revision logic
+      # At this point you can check:
+      #   - the data inserted,
+      #   - the final values ​​in the hash data, or
+      #   - perform some required process
+      def after_make row
       end
   end
 end
