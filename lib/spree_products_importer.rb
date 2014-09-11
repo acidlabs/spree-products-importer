5d4e05be9ad179c8896e03e1c30b91a7ab35267f
mespina <mespina@acid.cl>
2014-09-11 17:17:14 -0300

    Agrego require de archivos parsers y mappers

--- lib/spree_products_importer.rb
+++ lib/spree_products_importer.rb
@@ -1,6 +1,15 @@
 require 'spree_core'
 require 'spree_products_importer/engine'
 require 'spree_products_importer/handler'
+
 require 'spree_products_importer/mappers/base_mapper'
 require 'spree_products_importer/mappers/product_mapper'
+require 'spree_products_importer/mappers/taxon_mapper'
+require 'spree_products_importer/mappers/option_value_mapper'
+require 'spree_products_importer/mappers/aditional_mapper'
+
+require 'spree_products_importer/parsers/array_parser'
+require 'spree_products_importer/parsers/boolean_parser'
+require 'spree_products_importer/parsers/date_time_parser'
+
 require 'spree_products_importer/importer'
